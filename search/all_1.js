1162106e920a3f7ef5435ee1d46028056a9dc69f
Hanqi Guo <hguo@anl.gov>
2019-03-25 11:02:26 -0500

    updating docs

--- search/all_1.js
+++ search/all_1.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['basic_5finterval',['basic_interval',['../structftk_1_1basic__interval.html',1,'ftk']]],
-  ['branch_5fdecomposition_5ftree',['branch_decomposition_tree',['../structftk_1_1branch__decomposition__tree.html',1,'ftk']]]
+  ['basic_5finterval',['basic_interval',['../structftk_1_1basic__interval.html',1,'ftk']]]
 ];
